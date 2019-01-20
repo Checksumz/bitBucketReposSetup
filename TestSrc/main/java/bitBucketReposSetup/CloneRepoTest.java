4b20c61f1b41e986dc4f580d9063def983bccf8a
Amit DSOUZA <amit.dsouza@amadeus.com>
2019-01-20 22:16:36 +1100

    update

---
+++ TestSrc/main/java/bitBucketReposSetup/CloneRepoTest.java
@@ -0,0 +1,27 @@
+package main.java.bitBucketReposSetup;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CloneRepoTest {
+
+	private CloneRepo test;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
